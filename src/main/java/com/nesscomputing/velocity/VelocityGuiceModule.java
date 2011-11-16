dbdecb3f7a27bd039d14dfe6b5f5a5876ebef74e
Steven Schlansker <stevenschlansker@gmail.com>
2011-11-16 11:31:51 -0800

    Correct transitive dependencies so dep:analyze passes.

--- src/main/java/com/nesscomputing/velocity/VelocityGuiceModule.java
+++ src/main/java/com/nesscomputing/velocity/VelocityGuiceModule.java
@@ -6,7 +6,7 @@ import java.net.URISyntaxException;
 import java.nio.charset.Charset;
 import java.util.List;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.VFS;
