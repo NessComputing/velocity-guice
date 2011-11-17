bccafccaf084f255dc6dbfebd450c5fa1b8e580a
Steven Schlansker <steven@likeness.com>
2011-11-16 21:55:53 -0800

    Switch to slf4j so that we build without Ness specific infrastructure.

--- src/main/java/com/nesscomputing/velocity/CommonsVfsResourceLoader.java
+++ src/main/java/com/nesscomputing/velocity/CommonsVfsResourceLoader.java
@@ -1,7 +1,5 @@
 package com.nesscomputing.velocity;
 
-import io.trumpet.log.Log;
-
 import java.io.InputStream;
 
 import org.apache.commons.collections.ExtendedProperties;
@@ -11,10 +9,12 @@ import org.apache.commons.vfs2.VFS;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.resource.Resource;
 import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommonsVfsResourceLoader extends ResourceLoader {
 
-    private static final Log LOG = Log.findLog();
+    private static final Logger LOG = LoggerFactory.getLogger(CommonsVfsResourceLoader.class);
 
     @Override
     public void init(ExtendedProperties configuration) {
@@ -27,7 +27,7 @@ public class CommonsVfsResourceLoader extends ResourceLoader {
             FileSystemOptions opts = new FileSystemOptions();
             return VFS.getManager().resolveFile(source, opts).getContent().getInputStream();
         } catch (FileSystemException e) {
-            LOG.debug(e, "Could not load resource \"%s\"", source);
+            LOG.debug(String.format("Could not load resource \"%s\"", source), e);
             throw new ResourceNotFoundException(source, e);
         }
     }
